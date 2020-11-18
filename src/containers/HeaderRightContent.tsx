e897847915822b34a5c00fb762cbcc31f82d7a04
Konstantin Zv <kozzztya@gmail.com>
2020-11-18 11:51:28 +0200

    Fix nil issue

--- src/containers/HeaderRightContent.tsx
+++ src/containers/HeaderRightContent.tsx
@@ -1,10 +1,11 @@
+import _ from 'lodash';
 import React, { Fragment } from 'react';
 import { useGetBalanceQuery } from '../generated/apollo';
 
 export const HeaderRightContent = () => {
   const { data: balanceData } = useGetBalanceQuery();
 
-  if (!balanceData?.balance) return <Fragment />;
+  if (_.isNil(balanceData?.balance)) return <Fragment />;
 
   return <Fragment>{`Points: ${balanceData?.balance}`}</Fragment>;
 };
