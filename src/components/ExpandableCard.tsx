8457794d187ca5a720226373a49efb093f6e4dbf
kozzztya <kozzztya@gmail.com>
2021-02-03 14:35:09 +0200

    Fix expandable card styles

--- src/components/ExpandableCard.tsx
+++ src/components/ExpandableCard.tsx
@@ -1,9 +1,10 @@
-import { Card, Typography, IconButton, Collapse } from '@material-ui/core';
+import { Card, Typography, IconButton, Collapse, Icon } from '@material-ui/core';
 import React from 'react';
 import { useToggle } from 'react-use';
 import styled from 'styled-components';
 import { ExpandMore, ExpandLess } from '@material-ui/icons';
 import { SpeedDialIcon } from '@material-ui/lab';
+import { grey } from '@material-ui/core/colors';
 
 export interface ExpandableCardProps {
   defaultExpanded?: boolean;
@@ -14,13 +15,17 @@ const TitleWrapper = styled.div<{ isExpanded: boolean }>`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin: ${(p) => (p.isExpanded ? '10px' : '6px')} 15px 6px 15px;
+  margin: ${(p) => (p.isExpanded ? '10px' : '6px')} 9px 6px 15px;
 
   cursor: pointer;
 `;
 
 const Content = styled.div`
   margin: 0 0 10px 15px;
+`;
+
+const IconStyled = styled(Icon)`
+  color: ${grey[400]};
 `;
 
 export const ExpandableCard: React.FC<ExpandableCardProps> = ({
@@ -36,12 +41,14 @@ export const ExpandableCard: React.FC<ExpandableCardProps> = ({
         <Typography>{title}</Typography>
 
         <IconButton aria-expanded={isExpanded} aria-label="show more" size="small">
-          <SpeedDialIcon
-            icon={<ExpandMore />}
-            openIcon={<ExpandLess />}
-            open={isExpanded}
-            aria-label="show more"
-          />
+          <IconStyled>
+            <SpeedDialIcon
+              icon={<ExpandMore />}
+              openIcon={<ExpandLess />}
+              open={isExpanded}
+              aria-label="show more"
+            />
+          </IconStyled>
         </IconButton>
       </TitleWrapper>
 
