2a5eb2f184e714412d3ce114eb2d4a9ad76c7990
kozzztya <kozzztya@gmail.com>
2021-04-13 10:11:40 +0300

    Fix Cannot read property 'yValue' of undefined

--- src/components/chart/chartDataComparator.ts
+++ src/components/chart/chartDataComparator.ts
@@ -9,8 +9,8 @@ export const chartDataComparator = <T extends Props>(
   { data: nextData }: T
 ) => {
   // Trying to quickly understand that data changed
-  if (prevData.length !== nextData.length) return false;
-  if (prevData[0].yValue !== nextData[0].yValue) return false;
+  if (prevData?.length !== nextData?.length) return false;
+  if (prevData[0]?.yValue !== nextData[0]?.yValue) return false;
 
   // Rerender only if content was changed, otherwise don't show change transition
   return JSON.stringify(prevData) === JSON.stringify(nextData);
